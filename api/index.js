37b31f5c0b3c7f24892391cf11d8fb75cee40c4b
Ben-Hur Santos Ott <bhrott@proton.me>
2023-12-26 21:57:21 -0300

    adding cookies

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 const app = require('express')();
 const { v4 } = require('uuid');
 
+app.use(require('cookie-parser')());
+
 app.get('/api', (req, res) => {
   res.end(`Hello! ${v4()}`);
 });
+
+app.get('/api/set-cookie', (req, res) => {
+  res.cookie('session', v4(), {
+    httpOnly: true,
+    secure: true,
+  });
+})
+
+app.get('/api/get-cookie', (req, res) => {
+  res.end(`session cookie: ${req.cookies.session}`);
+})
 
 module.exports = app;
